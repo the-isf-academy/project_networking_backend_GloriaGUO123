3197ef2349f1f1d4b1e02b21ca3dde78eee780d3
Gloria Guo <gloriaguo33@gmail.com>
2024-10-14 11:56:06 +0800

    #today: I worked on the MVP of my model, which is building the id field (self-built) and adding the emoji_input field and linking the acess_code using a ForeignKey to link it with the id of the canva. Also writing the method of the json_response. However, I was not able to link the two database using ForeignKey. # Next, I will continue to work on the MVP that I didnt finish, especially working on the ForeignKey part, if time perimits, working on the view.py MVP

--- app/models.py
+++ app/models.py
@@ -1,3 +1,37 @@
 # models.py
 
-from banjo.models import Model, StringField, IntegerField, FloatField, BooleanField
+from banjo.models import Model, StringField, IntegerField, FloatField, BooleanField, ForeignKey
+
+class Canva: 
+    like = IntegerField()
+    # view = IntegerField()
+    # created_time = StringField()
+    # popularity_percentage = FloatField()
+    # time_period = StringField()
+    # creator = StringField()
+
+    # def json_response(self):
+    #     return {
+    #         'id': self.id,
+    #         'like': self.like,
+    #         'created_time': self.created_time,
+    #         'popularity_percentage': self
+    #     }
+
+class Emoji:
+    input_emoji = StringField()
+    # username = StringField()
+    # x_coordinates = IntegerField()
+    # y_coordinates = IntegerField()
+    acess_code = ForeignKey(Canva, on_delete=Model.CASCADE)
+
+    # def json_response(self):
+    #     return {
+    #         'id': self.id,
+    #         'emoji': self.emoji,
+    #         'username': self.username,
+    #         'x_coordinates': self.x_coordinates,
+    #         'y_coordinates': self.y_coordinates,
+    #         'acess_code': self.acess_code
+    #     }
+
