696a6c4422118ad3b1f630e0e8e0790bdd175477
Gloria Guo <gloriaguo33@gmail.com>
2024-10-15 15:29:51 +0800

    #Today I finished the MVP of the emoji database and I am trying to build the database for the canva. #Next Continue to build the canva database and try to link the emojis to a canva and have the acess code of the emoji link with the id of the canva (which I did before).

--- app/models.py
+++ app/models.py
@@ -2,8 +2,26 @@
 
 from banjo.models import Model, StringField, IntegerField, FloatField, BooleanField, ForeignKey
 
+class Emoji(Model):
+    input_emoji = StringField()
+    username = StringField()
+    # x_coordinates = IntegerField()
+    # y_coordinates = IntegerField()
+    # access_code = 0
+
+    # def json_response(self):
+    #     return {
+    #         'id': self.id,
+    #         'input_emoji': self.input_emoji,
+    #         'username': self.username,
+    #         'x_coordinates': self.x_coordinates,
+    #         'y_coordinates': self.y_coordinates,
+    #         'access_code': self.access_code
+    #         }
+
 class Canva(Model): 
     like = IntegerField()
+    emojis = ForeignKey(Emoji)
     # view = IntegerField()
     # created_time = StringField()
     # popularity_percentage = FloatField()
@@ -17,21 +35,3 @@ class Canva(Model):
     #         'created_time': self.created_time,
     #         'popularity_percentage': self
     #     }
-
-class Emoji(Model):
-    input_emoji = StringField()
-    # username = StringField()
-    # x_coordinates = IntegerField()
-    # y_coordinates = IntegerField()
-    acess_code = ForeignKey(Canva)
-
-    # def json_response(self):
-    #     return {
-    #         'id': self.id,
-    #         'emoji': self.emoji,
-    #         'username': self.username,
-    #         'x_coordinates': self.x_coordinates,
-    #         'y_coordinates': self.y_coordinates,
-    #         'acess_code': self.acess_code
-    #     }
-
--- app/views.py
+++ app/views.py
@@ -2,4 +2,25 @@
 
 from banjo.urls import route_get, route_post
 from settings import BASE_URL
+from .models import Emoji
+
+@route_get(BASE_URL + 'all')
+def all_emojis(args):
+    emojis_list = []
+    for emoji in Emoji.objects.all():
+        emojis_list.append(emoji.json_response())
+    if emojis_list != []:
+        return {'Emojis': emojis_list}
+    elif emojis_list == []:
+        return {'Error': 'No emojis exist'}
+
+@route_post(BASE_URL + 'new', args={'emoji':str, 'username': str})
+def new_emoji(args):
+    new_emoji = Emoji(
+        input_emoji = args['emoji'],
+        username = args['username'],
+        )
+    new_emoji.save()
+    
+    return {'Emoji': new_emoji.json_response()}
 
