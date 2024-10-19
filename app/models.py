5be4e5294c1dca753059586340d173b9b0c2f3dc
Gloria Guo <gloriaguo33@gmail.com>
2024-10-19 18:49:50 +0800

    #Notes I decided to change my plan, where the user can create their own canva, because I feel like user want to share different canva with different people (E.g. one for friends, one for family), I finished add new canva and add new emoji and added all the neccessary error messages that I can currently think of.

--- app/models.py
+++ app/models.py
@@ -4,69 +4,68 @@ from banjo.models import Model, StringField, IntegerField, FloatField, BooleanFi
 
 class Canva(Model): 
     like = IntegerField()
-    # view = IntegerField()
-    # created_time = StringField()
-    # popularity_percentage = FloatField()
-    # time_period = StringField()
-    # creator = StringField()
+    view = IntegerField()
+    created_time = StringField()
+    popularity_percentage = FloatField()
+    time_period = StringField()
 
     def __str__(self):
         return f"id: {self.id}"
 
-    # def json_response(self):
-    #     return {
-    #         'id': self.id,
-    #         'emojis':self.emojis,
-    #         'like': self.like,
-    #         'created_time': self.created_time,
-    #         'popularity_percentage': self.popularity_percentage
-    #     }
+    def canva_json_response(self):
+        return {
+            'id': self.id,
+            # 'emojis':self.emojis,
+            'like': self.like,
+            'created_time': self.created_time,
+            'popularity_percentage': self.popularity_percentage,
+            'access_code': self.id
+        }
 
-    # def checking_accesscode(self, access_code):
-    #     if access_code == self.id:
-    #         return True
-    #     elif access_code != self.id:
-    #         return False
+    def checking_accesscode(self, access_code):
+        if access_code == self.id:
+            return True
+        elif access_code != self.id:
+            return False
 
 class Emoji(Model):
     emoji = StringField()
-    # username = StringField()
-    # x_coordinates = IntegerField()
-    # y_coordinates = IntegerField()
+    username = StringField()
+    x_coordinates = IntegerField()
+    y_coordinates = IntegerField()
     canva = ForeignKey(Canva, related_name='emojis')
 
     def __str__(self):
-        return f"canva: {self.canva} "
+        return f"canva: {self.canva}, emoji: {self.emoji},username: {self.username},x: {self.x_coordinates}, y: {self.y_coordinates} "
 
-    # def json_response(self):
-    #     return {
-    #         'id': self.id,
-    #         'emoji': self.emoji,
-    #         'username': self.username,
-    #         'x_coordinates': self.x_coordinates,
-    #         'y_coordinates': self.y_coordinates,
-    #         }
+    def emoji_json_response(self):
+        return {
+            'id': self.id,
+            'emoji': self.emoji,
+            'username': self.username,
+            'x_coordinates': self.x_coordinates,
+            'y_coordinates': self.y_coordinates,
+            'access_code': self.canva.id
+            }
     
-    #need to check
-    # def setting_position(self):
-    #     last_emoji = Emoji.objects.last()
-    #     # The size of the canva is 10x10
-    #     if last_emoji:
-    #         if last_emoji.x_coordinates < 10:
-    #             # Keeps adding to the value of the x coordinate until reaches 10
-    #             self.x_coordinates = last_emoji.x_coordinates + 1
-    #             self.y_coordinates = last_emoji.y_coordinates
-    #             # return True
-    #         elif last_emoji.x_coordinates >= 10:
-    #             # Moves to the next column (y coordinate), as the x has reaches the maximum of 10
-    #             self.x_coordinates = 0
-    #             self.y_coordinates = last_emoji.y_coordinates +1
-    #             # return True
-    #         elif last_emoji.y_coordinates >= 10:
-    #         # Reaches the maximum size of the canva (10x10)
-    #             return False
-    #     else:
-    #         # if the emoji is the first one
-    #         self.x_coordinates = 0
-    #         self.y_coordinates = 0
+    def setting_position(self, access_code):
+        # The size of the canva is 3x3
+        last_emoji = Emoji.objects.filter(canva=access_code).last()
+        if last_emoji:
+            if last_emoji.x_coordinates <3:
+                # Keeps adding to the value of the x coordinate until reaches 3
+                self.x_coordinates = last_emoji.x_coordinates + 1
+                self.y_coordinates = last_emoji.y_coordinates
+            elif last_emoji.x_coordinates >=3:
+                # Moves to the next column (y coordinate), as the x has reaches the maximum of 3
+                self.x_coordinates = 0
+                self.y_coordinates = last_emoji.y_coordinates +1
+        else:
+            # If no emojis exist, initialize position
+            self.x_coordinates = 0
+            self.y_coordinates = 0
+        if self.y_coordinates >=4:
+            return False  # Indicate failure
+        # Check if the new position is still within the canvas limits
+        return True  # Indicate success
     
--- app/views.py
+++ app/views.py
@@ -15,21 +15,21 @@ from .models import Emoji, Canva
 #     elif emojis_list == []:
 #         return {'Error': 'No emojis exist currently'}
 
-@route_get(BASE_URL + 'all/emoji', args = {'access_code':int})
-def all_emojis(args):
-    for canva in Canva.objects.all():
-        if canva.checking_accesscode(args['access_code']) == True:
-            emojis_list = []
-            user_canva = Canva.objects.get(id=args['access_code'])
-            print(user_canva)
-            for emoji in user_canva.emojis:
-                emojis_list.append(emoji.json_response())
-            if emojis_list != []:
-                return {'Emojis': emojis_list}
-            elif emojis_list == []:
-                return {'Error': 'No emojis exist currently'}
-        elif canva.checking_accesscode(args['access_code']) == False:
-                return {'Error': 'Access Code Incorrect'}
+# @route_get(BASE_URL + 'all/emoji', args = {'access_code':int})
+# def all_emojis(args):
+#     for canva in Canva.objects.all():
+#         if canva.checking_accesscode(args['access_code']) == True:
+#             emojis_list = []
+#             user_canva = Canva.objects.get(id=args['access_code'])
+#             print(user_canva)
+#             for emoji in user_canva.emojis:
+#                 emojis_list.append(emoji.json_response())
+#             if emojis_list != []:
+#                 return {'Emojis': emojis_list}
+#             elif emojis_list == []:
+#                 return {'Error': 'No emojis exist currently'}
+#         elif canva.checking_accesscode(args['access_code']) == False:
+#                 return {'Error': 'Access Code Incorrect'}
 
 # @route_get(BASE_URL + 'all/canva')
 # def all_canvas(args = {'accesscode':int}):
@@ -41,17 +41,33 @@ def all_emojis(args):
 #         elif emojis_list == []:
 #             return {'Error': 'No canvas exist currently'}
 
-@route_post(BASE_URL + 'new', args={'emoji':str, 'username': str})
+@route_post(BASE_URL + 'new/emoji', args={'emoji':str, 'username': str, 'access_code':int})
 def new_emoji(args):
-    new_emoji = Emoji(
-        emoji = args['emoji'],
-        username = args['username'],
-        )
-    new_emoji.setting_position()
-    new_emoji.save()
-    return {'Emoji': new_emoji.json_response()}
-    # if new_emoji.setting_position() == True:
-    #     return {'Emoji': new_emoji.json_response()}
-    
-    # elif new_emoji.setting_position == False:
-    #     return {'Error': 'The canva size has reached to its maximum'}
+    if Canva.objects.filter(id=args['access_code']).exists() == True:
+        selected_canva = Canva.objects.get(id=args['access_code'])
+        new_emoji = Emoji(
+            emoji = args['emoji'],
+            username = args['username'],
+            canva = selected_canva
+            )
+        new_emoji.setting_position(args['access_code'])
+        new_emoji.save()
+        if new_emoji.setting_position(args['access_code']) == True:
+            return {'Emoji': new_emoji.emoji_json_response()}
+        
+        elif new_emoji.setting_position(args['access_code']) == False:
+            return {'Error': 'The canva size has reached to its maximum'}
+    elif Canva.objects.filter(id=args['access_code']).exists() == False:
+        return {'Error': 'Access code does not exit'}
+
+@route_post(BASE_URL + 'new/canva', args = {'current_time': str})
+def new_canva(args):
+    new_canva = Canva(
+        like = 0,
+        view = 0,
+        created_time = args['current_time'],
+        popularity_percentage = 0.0,
+        time_period = ''
+    )
+    new_canva.save()
+    return {'Canva': new_canva.canva_json_response()}
